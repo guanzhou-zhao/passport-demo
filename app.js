4896b2d8874f76e6037bbf99b4736350628f31fd
Guanzhou Zhao <benzhao2866@gmail.com>
2016-09-16 08:09:21 +1200

    Add passport (Incomplete)

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var expressSession = require('express-session')
 var config = require('./knexfile')[process.env.NODE_ENV || 'development']
 var knex = require('knex')(config)
 var KnexSessionStore = require('connect-session-knex')(expressSession)
-
+var flash = require('connect-flash')
+var passport = require('passport')
+var LocalStrategy = require('passport-local').Strategy;
 var fs = require('fs')
 var app = express();
 
@@ -15,16 +17,42 @@ var sess = {
   store: new KnexSessionStore({ knex: knex })
 }
 app.use(expressSession(sess))
+app.use(flash())
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }
+));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function (req, res) {
   // fs.writeFile('request.json', req.IncomingMessage, (err) => {
   //   if (err) throw err;
   //   console.log('It\'s saved!');
-console.log(req.sessionStore.tablename);
-console.log(req.sessionStore.knex === knex);
-    res.send('Hello World!');
+  console.log(req)
+  res.send('Hello World!');
   // });
 });
-
+app.get('/login', function (req, res, next) {
+  res.render('login')
+})
+app.post('/login',
+  passport.authenticate('local', {
+      successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true })
+);
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
