39902307221bbdc65a6f1f623253ef31752466ec
Guanzhou Zhao <benzhao2866@gmail.com>
2016-09-16 00:03:20 +1200

    Add session and knexStore for it

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 var express = require('express');
+var expressSession = require('express-session')
+var config = require('./knexfile')[process.env.NODE_ENV || 'development']
+var knex = require('knex')(config)
+var KnexSessionStore = require('connect-session-knex')(expressSession)
+
 var fs = require('fs')
 var app = express();
 
+var sess = {
+  resave: false,
+  saveUninitialized: false,
+  secret: 'keyboard cat',
+  cookie: {},
+  store: new KnexSessionStore({ knex: knex })
+}
+app.use(expressSession(sess))
 app.get('/', function (req, res) {
   // fs.writeFile('request.json', req.IncomingMessage, (err) => {
   //   if (err) throw err;
