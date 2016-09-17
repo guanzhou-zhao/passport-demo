5c6f2599aef8bbf50e979e45205bac6877c491c9
Guanzhou Zhao <benzhao2866@gmail.com>
2016-09-18 10:49:16 +1200

    Add passport.js

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var exphbs = require('express-handlebars')
 var expressSession = require('express-session')
+var bodyParser = require('body-parser')
+var ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn
 var config = require('./knexfile')[process.env.NODE_ENV || 'development']
 var knex = require('knex')(config)
 var KnexSessionStore = require('connect-session-knex')(expressSession)
@@ -7,8 +10,11 @@ var flash = require('connect-flash')
 var passport = require('passport')
 var LocalStrategy = require('passport-local').Strategy;
 var fs = require('fs')
+var _ = require('lodash')
 var app = express();
-
+app.use( bodyParser.urlencoded({ extended: true }) );
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 var sess = {
   resave: false,
   saveUninitialized: false,
@@ -18,34 +24,43 @@ var sess = {
 }
 app.use(expressSession(sess))
 app.use(flash())
-
+var users = [
+  {id: 1, username: 'ben', password: '123'},
+  {id: 2, username: 'haha', password: '234'}
+]
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    User.findOne({ username: username }, function (err, user) {
-      if (err) { return done(err); }
-      if (!user) {
-        return done(null, false, { message: 'Incorrect username.' });
-      }
-      if (!user.validPassword(password)) {
-        return done(null, false, { message: 'Incorrect password.' });
-      }
-      return done(null, user);
-    });
+    console.log('in LocalStrategy......');
+    var user = _.find(users, function (u) {
+      return (u.username === username && u.password === password)
+    })
+    if (!user) {
+      return done(null, false, { message: 'Incorrect username or password.' });
+    }
+    return done(null, user);
   }
 ));
+passport.serializeUser(function(user, done) {
+  console.log('serialize user......');
+  done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+  console.log('deserialize user......');
+  var duser = _.find(users, function (u) {
+    return u.id === user.id
+  })
+    done(null, duser);
+});
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/', function (req, res) {
-  // fs.writeFile('request.json', req.IncomingMessage, (err) => {
-  //   if (err) throw err;
-  //   console.log('It\'s saved!');
-  console.log(req)
-  res.send('Hello World!');
-  // });
+app.get('/', ensureLoggedIn(), function (req, res) {
+  console.log('req', req);
+  res.render('index')
 });
 app.get('/login', function (req, res, next) {
-  res.render('login')
+  res.render('login', {flash: req.flash('error')})
 })
 app.post('/login',
   passport.authenticate('local', {
@@ -53,6 +68,10 @@ app.post('/login',
     failureRedirect: '/login',
     failureFlash: true })
 );
+app.get('/logout', function (req, res, next) {
+  req.logout()
+  res.redirect('/login')
+})
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
